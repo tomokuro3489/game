0514cf1c505d58d12c0cce08a460b06b01748609
tomokisato <tomo.kuro.3489@gmail.com>
2022-05-20 21:08:46 +0900

    ○○.index.erbを修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,9 @@
 class HomesController < ApplicationController
- def  index 
- end
+    def index
+        @register = Register.all.sort {|a,b| b.liked_users.count <=> a.liked_users.count}.first(3)
+      end 
  
+      def show
+        @register = Register.find(params[:id])
+      end
 end
