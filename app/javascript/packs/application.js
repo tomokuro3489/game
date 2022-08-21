63647e474170d4b2f44e21ce093ecc0859a80092
tomokisato <tomo.kuro.3489@gmail.com>
2022-08-21 14:10:58 +0900

    ○○.index.erbを修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,23 @@ window.onload = function () {
 
 })}
 
+/*
+
+Visit My Website to see creative design
+        www.HUSAMUI.com
+
+
+*/
+
+
+$(document).ready(function(){
+  
+    $(".dropdown").click(function(){
+      $(".menu").toggleClass("showMenu");
+        $(".menu > li").click(function(){
+          $(".dropdown > p").html($(this).html());
+            $(".menu").removeClass("showMenu");
+        });
+    });
+    
+  });
